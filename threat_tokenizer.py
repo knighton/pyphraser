da99c71825ef4d980327d11c8b33110c97af0a6b
James Knighton <iamknighton@gmail.com>
2015-04-15 16:27:58 -0700

    Tokenizer for threats.

---
+++ threat_tokenizer.py
@@ -0,0 +1,26 @@
+class ThreatTokenizer(object):
+    """
+    Tokenization for the narrow goal of matching threat phrases.
+    """
+
+    def __init__(self):
+        self.expand = {
+            "'s": 'is',
+            "'d": 'would',
+        }
+
+    def tokenize(self, text):
+        """
+        Text -> tokens.
+        """
+        ss = text.split()
+        rr = []
+        for s in ss:
+            last_two = s[-2:]
+            split_out = self.expand.get(last_two)
+            if split_out is None:
+                rr.append(s[:-2])
+                rr.append(split_out)
+            else:
+                rr.append(s)
+        return rr

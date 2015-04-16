0ceee31cea82ac2025673b7c33f5fd09dc12d06e
James Knighton <iamknighton@gmail.com>
2015-04-15 18:30:41 -0700

    Add separate threat config.

---
+++ threat_config.py
@@ -0,0 +1,103 @@
+"""
+Configuration for threat detection.
+
+Threat detection looks for sequences of the form
+
+    (subject) (auxiliary verb) (adverb) (main verb)
+
+which are defined below.
+"""
+
+from enum import enum
+
+
+def lines(s):
+    """
+    Block of text -> lines with comments removed.
+    """
+    ss = s[1:-1].split('\n')
+    for i, s in enumerate(ss):
+        x = s.find('#')
+        if x != -1:
+            ss[i] = s[:x]
+    return map(lambda s: s.strip(), ss)
+
+
+def annotated_lines(enum, s):
+    """
+    Block of text -> lines with comments removed and annotations extracted.
+    """
+    patterns = []
+    annotations = []
+    for line in lines(s):
+        pattern, annotation = line.split(' -- ')
+        patterns.append(pattern)
+        annotation = enum.from_s[annotation.upper()]
+        annotations.append(annotation)
+    return patterns, annotation
+
+
+RoughSubject = enum('SubjectType: I_OR_WE RELIGIOUS')
+
+
+SUBJECTS, SUBJECT_TYPES = annotated_lines(RoughSubject, """
+    (first person subject) -- i_or_we
+    god -- religious
+    satan -- religious
+    allah -- religious
+""")
+
+
+Actuality = enum('Actuality: FUTURE DESIRE CONDITIONAL HYPOTHETICAL')
+
+
+AUX_VERBS, AUX_VERB_TYPES = annotated_lines(Actuality, """
+    (to be) going to -- future
+    (to be) planning to -- future
+    (to plan) to -- future
+    (to promise) (first person subject) (to be) going to -- future
+    (to promise) (third person subject) (to be) going to -- future
+    (to promise) (first person subject) (to be) gonna -- future
+    (to promise) (third person subject) (to be) gonna -- future
+    (to promise) (first person subject) will -- future
+    (to promise) (third person subject) will -- future
+    (to swear) (first person subject) (to be) going to -- future
+    (to swear) (third person subject) (to be) going to -- future
+    (to swear) (first person subject) (to be) gonna -- future
+    (to swear) (third person subject) (to be) gonna -- future
+    (to swear) (first person subject) will -- future
+    (to swear) (third person subject) will -- future
+    (to want) to -- desire
+    wanna -- desire
+    will -- future
+    would -- conditional
+    would like to -- hypothetical
+    would love to -- hypothetical
+    would so -- conditional
+    would want to -- conditional
+""")
+
+
+ADVERBS = lines("""
+    # This line is intentionally blank.
+    always
+""")
+
+
+MAIN_VERBS = lines("""
+    abduct
+    choke
+    kick (nonfirst possessive pronoun)
+    kill
+    make (nonfirst person object) a martyr
+    make an example of
+    punch
+    rape
+    steal
+    sucker punch
+    rip (nonfirst possessive pronoun)
+    slap
+    smack
+    threaten
+    tie
+""")

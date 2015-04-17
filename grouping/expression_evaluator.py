321e84fc4df2e38d693e4fee2978183d3c7363fc
James Knighton <iamknighton@gmail.com>
2015-04-16 19:47:11 -0700

    Add get_filters(), each_token_with_attrs() stubs to ExpressionEvaluator.

--- grouping/expression_evaluator.py
+++ grouping/expression_evaluator.py
@@ -1,4 +1,4 @@
-class ExpressionEvaluator(object):
+
     """
     Has a black box that yields tokens and their attributes and the knowledge of
     what the filters are regarding those attributes.
@@ -7,8 +7,8 @@ class ExpressionEvaluator(object):
     just the selected.
     """
 
-    def __init__(self, token_oracle, dimension2filters_values):
-        self._token_oracle = token_oracle
+    def __init__(self):
+        dimension2filters_values = self.get_filters()
 
         self._dimension2filter2value = {}
         for dimension, filters_values in dimension2filters_values.iteritems():
@@ -24,6 +24,30 @@ class ExpressionEvaluator(object):
                 values_seen.add(filter_value)
                 self._filter2dimension[filter_name] = dimension
                 self._filter2value[filter_name] = value
+
+    def get_filters(self):
+        """
+        () -> dict of dimension to list of (filter name, filter value).
+
+        Example:
+
+            Dimension:    'number'
+            Filter name:  'sing'
+            Filter value: Number.SING
+        """
+        raise NotImplementedError
+
+    def each_token_with_attrs(self, args):
+        """
+        Expression's args -> yields (token, token attributes)
+
+        Yields all possible tokens in its group with all their attributes, so
+        the caller can filter them according to a token generation expression.
+
+        Token group sizes are small, and this is done during preprocessing, so
+        it doesn't need to be screaming fast.
+        """
+        raise NotImplementedError
 
     def _is_match(self, have_dim2value, filter_dim2values):
         for have_dim, have_value in have_dim2value.iteritems():
@@ -42,8 +66,7 @@ class ExpressionEvaluator(object):
             value = self._filter2value[filter_name]
             filter_dim2values[dimension].append(value)
 
-        for token, have_dim2value in \
-                self.token_oracle.each_with_attrs(expr.args):
+        for token, have_dim2value in self.each_token_with_attrs(expr.args):
             if self._is_match(have_dim2value, filter_dim2values):
                 yield token
 

7a553753e91fbddb944c00bfaf0dd7ba77156e4e
James Knighton <iamknighton@gmail.com>
2015-04-16 19:53:15 -0700

    Personal pronoun/possessive determiner expression evaluators.

---
+++ grouping/personal_expression_evaluators.py
@@ -0,0 +1,69 @@
+from copy import deepcopy
+
+from grouping.expression_evaluator import ExpressionEvaluator
+
+
+PERS_PRO_FILTERS = {
+    'case': [
+        ('subj', PersProCase.SUBJ),
+        ('obj',  PersProCase.OBJ),
+        ('refl', PersProCase.REFL),
+    ],
+    'poss': [
+        ('notpos', Poss.NO),
+        ('pos',    Poss.YES),
+    ],
+    'number': [
+        ('sing', Number.SING),
+        ('plur', Number.PLUR),
+    ],
+    'person': [
+        ('1st', Person.FIRST),
+        ('2nd', Person.SECOND),
+        ('3rd', Person.THIRD),
+    ],
+    'personhood': [
+        ('thing',  Personhood.NO),
+        ('person', Personhood.YES),
+    ],
+    'gender': [
+        ('male', Gender.MALE),
+        ('female', Gender.FEMALE),
+        ('neuter', Gender.NEUTER),
+    ],
+}
+
+
+POS_DET_FILTERS = deepcopy(PERS_PRO_FILTERS)
+del POS_DET_FILTERS['case']
+del POS_DET_FILTERS['poss']
+
+
+class PersonalPronounExpressionEvaluator(ExpressionEvaluator):
+    def __init__(self, personals_mgr):
+        self._personals_mgr = personals_mgr
+        self._filters = PERS_PRO_FILTERS
+
+    def get_filters(self):
+        return self._filters
+
+    def each_with_attrs(self, args):
+        assert not args
+        for token, ppi in self._personals_mgr.each_pronoun_with_attrs():
+            yield token, ppi.to_d()
+
+
+class PersonalDeterminerExpressionEvaluator(ExpressionEvaluator):
+    def __init__(self, personals_mgr):
+        self._personals_mgr = personals_mgr
+        self._filters = POS_DET_FILTERS
+
+    def get_filters(self):
+        return self._filters
+
+    def each_with_attrs(self, args):
+        assert not args
+        for token, pdi in self._personals_mgr.each_determiner_with_attrs():
+            yield token, pdi.to_d()
+
+

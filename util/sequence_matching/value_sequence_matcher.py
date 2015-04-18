1d61398b2cf26e693295ee34d361f0c259445bbc
James Knighton <iamknighton@gmail.com>
2015-04-18 12:28:15 -0700

    Fix ValueSequenceMatcher.

--- util/sequence_matching/value_sequence_matcher.py
+++ util/sequence_matching/value_sequence_matcher.py
@@ -13,4 +13,4 @@ class ValueSequenceMatcher(SequenceMatcher):
         return item_we_have == value_to_match
 
     def _get_possible_options(self, block_index, item_we_have):
-        return self._value2topionxx_per_block[block_index].get(item_we_have, [])
+        return self._value2optionxx_per_block[block_index].get(item_we_have, [])
